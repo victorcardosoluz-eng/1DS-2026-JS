2b38a917618c0cf0e491c4b1b481fbdab5dbf350
victorcardosoluz-eng <victor.cardoso.luz@escola.pr.gov.br>
2026-05-18 09:03:46 -0300

    Add files via upload

---
+++ Aula01.js
@@ -0,0 +1,10 @@
+//alert("o carpa capim fica batendo no amiguinho!!!")
+
+console.log("Primeiro còdigo em JS");
+console.log(4+5);
+console.log(3 * 4);
+console.log( true );
+console.log( 3,89 );
+
+console.log( "Victor" + " " +23+" " + 1.83+" "+ true );
+
---
+++ Aula02.js
@@ -0,0 +1,27 @@
+//definição de variaveis
+
+let pessoa = "nome da pesssoa";
+let idadepessoa = 30;
+let alturapessoa = 1.93;
+let statuspessoa = true;
+let salariopessoa = 1500;
+let salarioBonus = 100;
+
+" let salarioTotal = salariopessoa + salarioBonus;
+
+console.log( pessoa );
+console.log( idadepessoa );
+console.log( alturapessoa );
+console.log( statuspessoa );
+console.log( salarioTotal );
+console.log( salarioBonus );
+console.log( salarioTotal);
+
+//UNINDO TODAS AS INFORMAÇÕES EM UM SÓ TEXTO:
+console.log(
+    'O nome digitado foi: '+pessoa+
+    ' a idade digitada foi: '+idadepessoa+
+    ' a altura digitada foi: '+alturapessoa+
+    ' o status digitado foi: '+statuspessoa+
+    ' o salário total é: '+salarioTotal+ '!!!'
+)
