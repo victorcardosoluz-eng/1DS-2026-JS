033d80549bc079f76164129d3a20d7cad92f2c90
victorcardosoluz-eng <victor.cardoso.luz@escola.pr.gov.br>
2026-05-18 09:21:14 -0300

    Add files via upload

---
+++ Aula03.js
@@ -0,0 +1,20 @@
+let nome == "Victor"
+
+if (nome == "Victor") {
+    console.log("Você digitou o nome correto!")
+}else{
+    console.log("Você digitou outra coisa...")
+}
+7
+let idade = 17;
+
+if (idade == 18) {
+        console.log("Você é praticamente adulto...")
+}else if(idade < 18 ){
+    console.log("Você ainda é de menor...")
+}else {
+    console.log("Você é adulto...") 
+}
+
+//FAÇA UMA VERFICAÇÃO COM IF E ELSE
+// DE UMA PESSOA SE ELA PODE OU NÃO VOTAR.
